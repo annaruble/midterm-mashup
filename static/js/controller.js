b98ee9d3221e72f4c054887671ec9d3af5cfb6b5
Anna Ruble <rublan01@annas-mbp.lc.luther.edu>
2018-04-16 19:50:20 -0500

    adding button - enter click and score error handling

--- static/js/controller.js
+++ static/js/controller.js
@@ -1,8 +1,19 @@
+let titleIn = document.getElementById("titleInput")
+let yearIn = document.getElementById("yearInput")
+
+titleIn.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        document.getElementById("enter").click();
+    }
+})
+
 function search() {
     let config = {};
+
     let title = document.getElementById("titleInput").value;
     let pTitle = title.replace(/ /g, "+")
     let year = document.getElementById("yearInput").value;
+
     fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=72961f58&t=${pTitle}&type=movie&y=${year}&plot=full`)
     .then(function(response) {
         console.log(response)
@@ -10,7 +21,6 @@ function search() {
     })
     .then(function(movieInfo) {
         console.log(x = movieInfo)
-        //let movieObj = JSON.parse(jsonStr)
         if (movieInfo["Response"] == "False") {
             alert("Your title was not found. Please try rephrasing or searching a new title.")
         }
@@ -32,7 +42,19 @@ function checkTitle(movieInfo) {
     document.getElementById("genre").innerHTML = movieInfo["Genre"]
     document.getElementById("actorsDir").innerHTML = movieInfo["Actors"] + " | Director: " + movieInfo["Director"]
     document.getElementById("plot").innerHTML = movieInfo["Plot"]
-    document.getElementById("Tscore").innerHTML = movieInfo["Ratings"][1]["Value"]
-    document.getElementById("Mscore").innerHTML = movieInfo["Ratings"][2]["Value"]
+
+    try {
+        document.getElementById("Tscore").innerHTML = movieInfo["Ratings"][1]["Value"]
+    }
+    catch(err) {
+        document.getElementById("Tscore").innerHTML = "N/A"
+    }
+
+    try {
+        document.getElementById("Mscore").innerHTML = movieInfo["Ratings"][2]["Value"]
+    }
+    catch(err) {
+        document.getElementById("Mscore").innerHTML = "N/A"
+    }
 
 }
